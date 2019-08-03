0cbba0e52e48ef5e561a4e63ffc6d79c62d781a3
NaraHiroyuki <hiroyuki0818yakyu@icloud.com>
2019-08-03 12:50:53 +0900

    add code

--- section4.php
+++ section4.php
@@ -53,7 +53,6 @@ for($i = 1; $i < 10; $i++) {
     for($j = 1; $j < 10; $j++) {
         $result = $i * $j; 
         if ($result > 40) {
-            echo "\n";
             break;
         }
         print "{$result} "; 
@@ -61,4 +60,53 @@ for($i = 1; $i < 10; $i++) {
     echo "\n";
 }
 
+for ($i = 1; $i < 10; $i++) {
+    for ($j = 1; $j < 10; $j++) {
+        $result = $i * $j;
+        if ($result > 40) {
+            break 2;  // breakの後に脱出するループの階層を指定する
+        }
+        print "{$result} ";
+    }
+    echo "\n";
+}
+
+// $iを1~3で変化させるループ
+for ($i = 1; $i <4; $i ++) {
+    $result = 0;
+    switch ($i) {
+        case 1 :
+        case 3 :
+            // $i = 1,3 の場合のみ変数$resultを計算
+            $result = $i * $i;
+            break;
+            case 2:
+                continue;
+    }
+    print "{$i}の2乗は{$result}です。";
+    echo "\n";
+}
+
+for ($i = 1; $i < 10; $i++) {
+    for ($j = 1; $j < 10; $j++) {
+        $result = $i * $j;
+        if ($result > 40) {
+            goto end;
+        }
+        print "{$result} ";
+    }
+    echo "\n";
+}
+end :
+echo "\n";
+
+// 別構文
+$x = 35;
+if ($x > 20):
+    print '変数$xは20より大きいです。';
+elseif ($x > 10):
+    print '変数$xは10より大きく、20,以下です。';
+else:
+    print '変数$xは10以下です。';
+endif;
 ?>
